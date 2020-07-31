65c50c4667e91377527306f06a1c6f0aaef9bef3
Looja <looja1999@gamil.com>
2020-08-01 00:38:14 +1000

    permission and token authentication

--- rest_api_app/views.py
+++ rest_api_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets 
 from rest_api_app.serializers import StudentSerializers
 from rest_api_app.models import Student
+from rest_framework.authentication import TokenAuthentication 
 
 # Create your views here.
 class StudentViewSets(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializers
+    authentication_class = TokenAuthentication
+  
